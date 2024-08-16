75e8fce48957f23d5aa39a51bb697692cccc4784
Karl Mårtensson <karlmartenssonsales@gmail.com>
2024-08-16 11:22:24 +0200

    same previous

--- webApi/index.js
+++ webApi/index.js
@@ -11,28 +11,28 @@ const express = require('express');
         title: "Inte klart med ersättare för Ribbenvik", 
         summary: "▸ Regeringen och SD har ännu inte hittat någon ersättare för Migrationsverkets avgående generaldirektör Mikael Ribbenvik.", 
         link: "https://www.aftonbladet.se/nyheter/a/8JWWL2/inte-klart-med-ersattare-for-ribbenvik", 
-        published: new Date(Date.now()), // 1 dag gammal
+        published: new Date(Date.now()),
         topic: ["SamhalleKonflikter"] 
     },
     { 
         title: "Drogs in i inhägnad – dödades av 40 krokodiler", 
         summary: "▸ En 72-årig man har dödats av omkring 40 krokodiler sedan han dragits in i en inhägnad på familjens reptilfarm.", 
         link: "https://www.aftonbladet.se/nyheter/a/bgWW6e/drogs-in-i-inhagnad-dodades-av-40-krokodiler", 
-        published: new Date(Date.now()), // 2 dagar gammal
+        published: new Date(Date.now()),
         topic: ["Ekonomi"] 
     },
     { 
         title: "Regionpolitiker: Mer stöd från staten behövs", 
         summary: "▸ Regeringen behöver skjuta till pengar för att välfärden inte ska drabbas drastiskt.", 
         link: "https://www.aftonbladet.se/nyheter/a/y6XXyR/regionpolitiker-mer-stod-fran-staten-behovs", 
-        published: new Date(Date.now()), // 1 dag gammal
+        published: new Date(Date.now()),
         topic: ["Politik"] 
     },
     { 
         title: "Två avlidna hittade i bostad i Sandviken", 
         summary: "▸ En man och en kvinna har hittats avlidna i en bostad i Sandviken, skriver polisen på sin hemsida. Dödsorsaken är oklar.", 
         link: "https://www.aftonbladet.se/nyheter/a/dwPPP1/tva-avlidna-hittade-i-bostad-i-sandviken", 
-        published: new Date(Date.now()), // 3 dagar gammal
+        published: new Date(Date.now()),
         topic: ["Miljo"] 
     }
 ];
